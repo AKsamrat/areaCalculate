ec4ac2fc99b1bfede46e1d33121b25a10da5e2e2
Aksamrat <samratuap52@gmail.com>
2024-02-14 12:24:25 +0600

    solve output display area name

--- area.js
+++ area.js
@@ -16,12 +16,43 @@ function setInputDisplay(inputId1, inputId2) {
   inputDisplay2.value = '';
 }
 
+function setAtribute(value) {
+  const areaName = document.getElementById('whichArea');
+  areaName.innerText = '';
+  const p = document.createElement('p');
+  if (value === 1) {
+    p.innerText = 'Triangle';
+    areaName.appendChild(p);
+  }
+  if (value === 2) {
+    p.innerText = 'Rectangle';
+    areaName.appendChild(p);
+  }
+  if (value === 3) {
+    p.innerText = 'Parallalogram';
+    areaName.appendChild(p);
+  }
+  if (value === 4) {
+    p.innerText = 'Rhombus';
+    areaName.appendChild(p);
+  }
+  if (value === 5) {
+    p.innerText = 'Pentagon';
+    areaName.appendChild(p);
+  }
+  if (value === 6) {
+    p.innerText = 'Ellipse';
+    areaName.appendChild(p);
+  }
+}
+
 function triangleArea() {
   const base = inputCalculation('triangle-base');
   const height = inputCalculation('triangle-height');
   const area = 0.5 * base * height;
   setOutputValue(area);
   setInputDisplay('triangle-base', 'triangle-height');
+  setAtribute(1);
 }
 function rectangleArea() {
   const base = inputCalculation('rectangal-width');
@@ -29,13 +60,15 @@ function rectangleArea() {
   const area = base * height;
   setOutputValue(area);
   setInputDisplay('rectangal-width', 'rectangal-length');
+  setAtribute(2);
 }
 function parallelogramArea() {
   const base = inputCalculation('parallelogram-base');
   const height = inputCalculation('parallelogram-height');
   const area = base * height;
   setOutputValue(area);
-  setInputDisplay('parallelogram-base', 'parallelogram-heighth');
+  setInputDisplay('parallelogram-base', 'parallelogram-height');
+  setAtribute(3);
 }
 
 function rhombusArea() {
@@ -44,6 +77,7 @@ function rhombusArea() {
   const area = 0.5 * base * height;
   setOutputValue(area);
   setInputDisplay('rhombus-base', 'rhombus-height');
+  setAtribute(4);
 }
 
 function pentagonArea() {
@@ -52,11 +86,14 @@ function pentagonArea() {
   const area = 0.5 * base * height;
   setOutputValue(area);
   setInputDisplay('pentagon-base', 'pentagon-height');
+  setAtribute(5);
 }
+
 function ellipseArea() {
   const base = inputCalculation('ellipse-base');
   const height = inputCalculation('ellipse-height');
   const area = 3.1416 * base * height;
   setOutputValue(area);
   setInputDisplay('ellipse-base', 'ellipse-height');
+  setAtribute(6);
 }
